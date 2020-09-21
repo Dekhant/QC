8c8baa37036f5d9499c5ee81b4690e9c5cb9578d
Dekhant <52196622+Dekhant@users.noreply.github.com>
2020-09-21 16:50:43 +0300

    add

--- lab1/TriangleCheck/Program.cs
+++ lab1/TriangleCheck/Program.cs
@@ -5,45 +5,75 @@ namespace TriangleCheck
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static bool CheckMaxDouble(double a, double b, double c)
         {
-            if(args.Length != 3)
+            return a > double.MaxValue || b > double.MaxValue || c > double.MaxValue;
+        }
+
+        static bool CheckNonpositiveNumber(double a, double b, double c)
+        {
+            return a <= 0 || b <= 0 || c <= 0;
+        }
+
+        static bool CheckTriangleExist(double a, double b, double c)
+        {
+            return a + b > c && b + c > a && a + c > b;
+        }
+
+        static bool CheckEquilateralTriangle(double a, double b, double c)
+        {
+            return a == b && a == c && b == c;
+        }
+
+        static bool CheckIsoscelesaTriangle(double a, double b, double c)
+        {
+            return a == b || b == c || a == c;
+        }
+
+        static void GetTriangleType(double a, double b, double c)
+        {
+            if (CheckMaxDouble(a, b, c) || CheckNonpositiveNumber(a, b, c))
             {
                 Console.WriteLine("неизвестная ошибка");
                 return;
             }
-            int i = 0;
-            double[] mas = new double[3];
-            foreach(var side in args)
+            else if (!CheckTriangleExist(a, b, c))
             {
-                bool success = double.TryParse(side, out mas[i]);
-                if(!success)
-                {
-                    Console.WriteLine("неизвестная ошибка");
-                    return;
-                }
-                i++;
+                Console.WriteLine("это не треугольник");
+                return;
             }
-            double perimeter = mas.Sum();
-            foreach(var side in mas)
-            {
-                if((perimeter - side) < side)
-                {
-                    Console.WriteLine("это не треугольник");
-                    return;
-                }
-            }
-            if(perimeter / 3 == mas[0])
+            else if (CheckEquilateralTriangle(a, b, c))
             {
                 Console.WriteLine("равносторонний");
                 return;
             }
-            if(mas[0] == mas[1] || mas[0] == mas[2] || mas[1] == mas[2])
+            else if (CheckIsoscelesaTriangle(a, b, c))
             {
                 Console.WriteLine("равнобедренный");
                 return;
             }
             Console.WriteLine("обычный");
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("неизвестная ошибка");
+                return;
+            }
+            try
+            {
+                double a = double.Parse(args[0]);
+                double b = double.Parse(args[1]);
+                double c = double.Parse(args[2]);
+
+                GetTriangleType(a, b, c);
+            }
+            catch
+            {
+                Console.WriteLine("неизвестная ошибка");
+            }
         }
     }
 }
